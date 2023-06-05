f5a2703575c1a382d92fbeca60d88a796aa1d5d4
Matheus Macedo Santos <matheus.macedo.developer@gmail.com>
2023-06-05 08:23:27 -0300

    Creating the list players feature

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,33 @@
+using complete_gamer_project.Infra;
+using Microsoft.AspNetCore.Mvc;
+
+namespace complete_gamer_project.Controllers
+{
+	[Route("[controller]")]
+	public class PlayerController : Controller
+	{
+		private Context context = new Context();
+		
+		private readonly ILogger<PlayerController> _logger;
+
+		public PlayerController(ILogger<PlayerController> logger)
+		{
+			_logger = logger;
+		}
+
+		[Route("List")]
+		public IActionResult Index()
+		{
+			ViewBag.Players = context.Players.ToList();
+			ViewBag.Teams = context.Teams.ToList();
+			
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View("Error!");
+		}
+	}
+}
